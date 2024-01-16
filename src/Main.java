8365867b964d3647f48c85a116dd0bad69db18f3
Владимир <vkuznecov1504@mail.ru>
2024-01-16 12:28:21 +0400

    С помощью цикла for вывели числа от 10 до 1 в консоль

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,11 @@ public class Main {
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
+
+        //Задача 2
+
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
     }
 }
