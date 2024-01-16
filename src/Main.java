d0515ad7e924b0564209a4a9f6888d5d35e1191b
Владимир <vkuznecov1504@mail.ru>
2024-01-16 15:26:15 +0400

    С помощью цикла for вывели определенную последовательность чисел в консоль

--- src/Main.java
+++ src/Main.java
@@ -30,5 +30,11 @@ public class Main {
         for (int i = 1904; i < 2096; i= i + 4) {
             System.out.println(i + " год является високосным");
         }
+
+        //Задача 6
+
+        for (int i = 7; i <= 98; i = i + 7) {
+            System.out.println(i);
+        }
     }
 }
