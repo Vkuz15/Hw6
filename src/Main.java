2e84b13d0124fdcedb7e7bfb62b3e4bb783f8552
Владимир <vkuznecov1504@mail.ru>
2024-01-16 12:41:22 +0400

    С помощью цикла for вывели все четные числа от 0 до 17 в консоль

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,11 @@ public class Main {
         for (int i = 10; i >= 1; i--) {
             System.out.println(i);
         }
+
+        //Задача 3
+
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println(i);
+        }
     }
 }
