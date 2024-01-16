442ba2dbe7620f655258594f85eefd1a051a0912
Владимир <vkuznecov1504@mail.ru>
2024-01-16 15:19:08 +0400

    С помощью цикла for вывели числа от 10 до -10 в консоль

--- src/Main.java
+++ src/Main.java
@@ -18,5 +18,11 @@ public class Main {
         for (int i = 0; i <= 17; i = i + 2) {
             System.out.println(i);
         }
+
+        //Задача 4
+
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(i);
+        }
     }
 }
